b47de593bd472700e699645c4815442ff170fe21
CarlosZ <zabaletacopa@gmail.com>
2024-12-03 14:22:55 -0400

    Adding Succes page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import Link from 'next/link'
 
 export default function Home() {
   const [checkedOption1, setCheckedOption1] = useState(false);
@@ -106,10 +107,9 @@ export default function Home() {
         </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          <Link
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            href="/success"
             rel="noopener noreferrer"
           >
             <Image
@@ -119,8 +119,8 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
-          </a>
+            Submit
+          </Link>
         </div>
       </main>
     </div>
